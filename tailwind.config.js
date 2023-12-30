f88758236b943b7a98af2b17fee5cd0426b89cc2
sunera Harshana <107272662+SuneraL77@users.noreply.github.com>
2023-12-30 15:03:06 +0530

    user login

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode:"class",
+  theme: {
+    extend: {
+      colors:{
+    
+        dark_bg_1: "#111B21",
+        dark_bg_2: "#202C33",
+        dark_bg_3: "#182229",
+        dark_bg_4: "#222E35",
+        dark_border_1: "#222D34",
+        dark_border_2: "#313D45",
+        dark_hover_1: "#2A3942",
+        dark_svg_1: "#AEBAC1",
+        dark_svg_2: "#8696A0",
+        dark_svg_2: "#53BDEB",
+      },
+    },
+  },
+  plugins: [],
+}
